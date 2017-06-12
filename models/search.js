15393e51049e37614245e7faa5a746008979d55c
Jakub Jablonski <kuba.jablonski.zg@gmail.com>
2017-06-12 15:39:12 +0200

    Add GET /latest/imagesearch route

--- models/search.js
+++ models/search.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 let Search = mongoose.model('Search', {
+    createdAt: {
+        type: Number,
+        required: true
+    },
     term: {
         type: String,
         required: true,
--- server.js
+++ server.js
@@ -17,26 +17,51 @@ app.get('/imagesearch/:searchquery', (req, res) => {
     let searchQuery = req.params.searchquery;
     let offset = req.query.offset || '0';
 
-    searchBing.get(`/images/search?q=${searchQuery}&offset=${offset}&count=10`).then((response) => {
-        let customRes = [];
-        response.data.value.forEach((value) => {
-            customRes.push({
-                name: value.name,
-                url: value.webSearchUrl,
-                thumbnail: value.thumbnailUrl,
-                host: value.hostPageDisplayUrl
+    searchBing.get(`/images/search?q=${searchQuery}&offset=${offset}&count=10`)
+        .then((response) => {
+            let customRes = [];
+            response.data.value.forEach((value) => {
+                customRes.push({
+                    name: value.name,
+                    url: value.webSearchUrl,
+                    thumbnail: value.thumbnailUrl,
+                    host: value.hostPageDisplayUrl
+                });
             });
-        });
 
-        res.status(200).send(customRes);
-    }).then(() => {
-        let search = new Search({
-            term: searchQuery,
-            when: moment().utc().format('MMMM Do YYYY, h:mm:ss a')
-        });
+            res.status(200).send(customRes);
+        })
+        .then(() => {
+            let now = moment().utc();
 
-        search.save()
-    }).catch((e) => res.send(e));
+            let search = new Search({
+                createdAt: +now,
+                term: searchQuery,
+                when: now.format('MMMM Do YYYY, h:mm:ss a')
+            });
+
+            search.save((e) => {
+                if (e) console.log(e);
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+            res.send();
+        });
+});
+
+app.get('/latest/imagesearch', (req, res) => {
+    Search
+        .find({})
+        .limit(10)
+        .sort('-createdAt')
+        .select({_id: 0, createdAt: 0, __v: 0})
+        .exec()
+        .then((docs) => res.send(docs))
+        .catch((e) => {
+            console.log(e);
+            res.send();
+        });
 });
 
 app.listen(port, () => {
